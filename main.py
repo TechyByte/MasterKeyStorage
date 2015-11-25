4a069975caef6c2c66d769588149aa3a9301abb9
George Hynes <info@techybyte.co.uk>
2015-11-25 21:15:43 +0000

    Initial Commit

--- main.py
+++ main.py
@@ -97,14 +97,14 @@ def openKey():
     locked = False
     sleep(1)
 
-    # TODO: Unlock key here
+    # TODO: Unlock key here!
 
     sleep(1)
     print("Scan a token or press # key to lock system")
     input("> ")
     sleep(1)
 
-    # TODO: lock key here
+    # TODO: lock key here!
 
     sleep(1)
     locked - True
