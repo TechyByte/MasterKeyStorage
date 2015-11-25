a52a3a1c36f0cf6ae62ec39359bc5e1e4cfce263
George Hynes <info@techybyte.co.uk>
2015-11-25 23:21:10 +0000

    Chec

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/bin/env python3.3
-# Software backend of Secure Storage of certain Master Keys
+# Software backend of Secure Storage of certain  Master Key
 # By George Hynes, TechyByte
 # Copyright (C) George Hynes and TechyByte 2015. All rights reserved.
 
